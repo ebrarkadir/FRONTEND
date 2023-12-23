ad151f2fe8bd92d8ff8244b986860a99160e5e86
Ebrar Kadir Çetin <cetinebrarkadir@gmail.com>
2023-12-23 18:44:31 +0300

    0.8

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -6,8 +6,14 @@ import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
   const content = (
-    <CSSTransition in={props.show}>
-      <aside className="side-drawer">{props.children}</aside>
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
     </CSSTransition>
   );
 
