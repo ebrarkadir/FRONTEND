c9b2a8ecbb7ae2e92e49a2b18db545a1c4703ad2
ebrarkadir <cetinebrarkadir@gmail.com>
2024-03-21 22:21:35 +0300

    updates

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -1,17 +1,23 @@
-import { userState, useCallback } from "react";
+import { userState, useCallback, useRef, useEffect } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = userState(false);
   const [error, setError] = userState();
 
+  const activeHttpRequests = useRef([]);
+
   const sendRequest = useCallback(
     async (url, method = "GET", body = null, headers = {}) => {
       setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
       try {
         const responce = await fetch(url, {
           method,
           body,
           headers,
+          signal: httpAbortCtrl.signal,
         });
 
         const responceData = await responce.json();
@@ -32,5 +38,11 @@ export const useHttpClient = () => {
     setError(null);
   };
 
-  return { isLoading, error, sendRequest, clearError};
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
 };
